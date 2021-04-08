709438d1859e3aeee89aa3d3d40778b69420f042
Aaron Pinto <10177767+AaronPinto@users.noreply.github.com>
2021-04-08 17:26:53 -0400

    set up test directory structure with symlink

--- Lab 5/main.cpp
+++ Lab 5/main.cpp
@@ -10,6 +10,17 @@
 #include <pwd.h>
 #include <sys/stat.h>
 
+void print_attrs(struct stat sb, char *pathname) {
+    printf("Mode:                     %lo (octal)\n", (unsigned long) sb.st_mode);
+    printf("Link count:               %ld\n", (long) sb.st_nlink);
+    printf("Owner's name:             %s\n", getpwuid(sb.st_uid)->pw_name);
+    printf("Group name:               %s\n", getgrgid(sb.st_gid)->gr_name);
+    printf("File size:                %lld bytes\n", (long long) sb.st_size);
+    printf("Blocks allocated:         %lld\n", (long long) sb.st_blocks);
+    printf("Last file modification:   %s", ctime(&sb.st_mtime));
+    printf("Name:                     %s", basename(strdup(pathname)));
+}
+
 int main(int argc, char *argv[]) {
     struct stat sb {};
 
@@ -26,17 +37,8 @@ int main(int argc, char *argv[]) {
     printf("File type:                ");
 
     switch (sb.st_mode & S_IFMT) {
-        case S_IFBLK:
-            printf("block device\n");
-            break;
-        case S_IFCHR:
-            printf("character device\n");
-            break;
         case S_IFDIR:
             printf("directory\n");
-            break;
-        case S_IFIFO:
-            printf("FIFO/pipe\n");
             break;
         case S_IFLNK:
             printf("symlink\n");
@@ -44,22 +46,12 @@ int main(int argc, char *argv[]) {
         case S_IFREG:
             printf("regular file\n");
             break;
-        case S_IFSOCK:
-            printf("socket\n");
-            break;
         default:
-            printf("unknown?\n");
+            printf("other\n");
             break;
     }
 
-    printf("Mode:                     %lo (octal)\n", (unsigned long) sb.st_mode);
-    printf("Link count:               %ld\n", (long) sb.st_nlink);
-    printf("Owner's name:             %s\n", getpwuid(sb.st_uid)->pw_name);
-    printf("Group name:               %s\n", getgrgid(sb.st_gid)->gr_name);
-    printf("File size:                %lld bytes\n", (long long) sb.st_size);
-    printf("Blocks allocated:         %lld\n", (long long) sb.st_blocks);
-    printf("Last file modification:   %s", ctime(&sb.st_mtime));
-    printf("Name:                     %s", basename(strdup(argv[1])));
+    print_attrs(sb, argv[1]);
 
     exit(EXIT_SUCCESS);
 }
